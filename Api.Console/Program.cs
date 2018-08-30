dcd72c1d7cdfe033da7c466a4e78c598c84fedfd
panicoenlaxbox <panicoenlaxbox@gmail.com>
2018-08-30 14:19:11 +0200

    Add project files.

---
+++ Api.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Api.Console
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new Client.Client("https://localhost:5001/");
+            var result = await client.ApiFooGetAsync();
+            foreach (var item in result)
+            {
+                System.Console.WriteLine($"{item.Bar}, {item.Baz}");
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ ApiSwashbuckle/Controllers/FooController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSwashbuckle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Foo>> Get()
+        {
+            return new[]
+            {
+                new Foo()
+                {
+                    Bar = "Bar", Baz = "Baz",
+
+                },
+                new Foo()
+                {
+                    Bar = "Bar", Baz = "Baz",
+                },
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Foo> Get([FromQuery] int id)
+        {
+            return new Foo()
+            {
+                Bar = "Bar",
+                Baz = "Baz",
+            };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Foo value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Foo value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
